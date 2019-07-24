d5b86546164714ac0ee35f88f29c16d189b1aaa1
Alex Reilly <areilly@yelp.com>
2019-07-24 14:01:48 -0700

    update to swift 5

--- Package.swift
+++ Package.swift
@@ -1,5 +1,17 @@
+// swift-tools-version:5.0
+
 import PackageDescription
 
 let package = Package(
-    name: "SwiftyGPIO"
+    name: "SwiftyGPIO",
+    products: [
+        .library(
+            name: "SwiftyGPIO",
+            targets: ["SwiftyGPIO"]),
+    ],
+    targets: [
+        .target(
+            name: "SwiftyGPIO",
+            dependencies: [])
+    ]
 )
